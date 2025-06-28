72cc636f421a784de81e4d0e8845a7eba04d982b
Connor <cjflowers@pm.me>
2025-06-28 13:24:29 +1000

    test primary colour in layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,13 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/**/*.{astro,html,js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        "primary": "#1F2937",
-        "secondary": "#F9FAFB",
-        "accent": "#34D399",
+        primary: "#1F2937",
+        secondary: "#F9FAFB",
+        accent: "#34D399",
         "accent-secondary": "#FB923C",
         "font-color": "#F9FAFB",
         "font-color-dark": "#1F2937",
